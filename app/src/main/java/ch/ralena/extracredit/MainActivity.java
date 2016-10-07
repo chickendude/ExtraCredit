aabeb12428ec363d5eea5fb9b4ad9fe830ceac46
Alessandro Oversluij <crater@localhost.localdomain>
2016-10-08 01:36:05 +0800

    Initial commit

---
+++ app/src/main/java/ch/ralena/extracredit/MainActivity.java
@@ -0,0 +1,16 @@
+package ch.ralena.extracredit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+	// TODO: ask user for their favorite color and show it to them
+	// TODO: design fake login screen and log them in fakely
+	// TODO: ask user to input age in years then show age in days, weeks, months, etc.
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
